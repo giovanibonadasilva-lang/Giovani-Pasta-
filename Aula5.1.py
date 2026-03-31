d26c5d0ad6de1f99e6183a60e720861d1d320159
giovanibonadasilva-lang <giovanibonadasilva@gmail.com>
2026-03-31 20:20:26 -0300

    Add files via upload

--- Aula5.1.py
+++ Aula5.1.py
@@ -1,12 +1,47 @@
-T=(int(input("escreva as horas")))
-preço=(int(10+5*T))
-P2=(int(20+8*T))
-if T<1:
-    print("Gratis")
+N1 = [7.5,8.0,6.5,9.0]
+print(N1)
+soma = sum(N1)
+print(soma)
+divisor=len(N1)
+R1 = (soma/divisor)
+print(R1)
+N1.append(5.0)
+print(N1)
+N1.pop(2)
+print(N1)
+Ordem = sorted(N1)
+print(Ordem)
+print(list(reversed(Ordem)))
+umatuple =(1,2,3,4)
+print(umatuple)
+list(umatuple)
+print(umatuple)
 
-if T>=1 and T<3:
-    print(10+5*T -5)
+T1 =(1,2)
+T2 =(3,4)
+doistuple =(T1 + T2)
+print(doistuple)
+umset = (1,3,3,6,5)
+print(set(umset))
+sett = {1,3,3,6,5}
+sett.add(2)
+sett.remove(6)
+print(sett)
 
-if T>=3:
-    N2=(int(T+P2))
-    print(20+8*T -8)
+S1 ={1,2,3}
+S2 ={3,4,5}
+print(S1 & S2)
+print(S1 | S2)
+print(S2 - S1)
+
+trestuple =(22,25,27,21)
+if max(trestuple) > 26:
+    print("o clima esta quente")
+
+quartoset =(2,4,6,8)
+print(quartoset)
+digitado = (int(input("escreva o numero")))
+if digitado in quartoset:
+      print(quartoset)
+
+
---
+++ Nova materia.py
@@ -0,0 +1,57 @@
+#                               função                         procedimento
+#               len   ()->  Tamanho                             globol -> geral
+#               range ()->  Criar lista                         def -> ele pode pegar os comando de outros arquivos
+#               sum   ()->  Soma                                
+# 
+#               Exeplos:
+#               
+#                def apresenta(nome):   
+#                    nome == "Giovani"
+#                    print(f"Olá, meu nome é {nome}!")
+#                apresenta()   
+#
+#                -----////////////////////////////-----  
+#                  
+#                def soma(a, b):    
+#                   return a + b 
+#                    
+#                a = (int("Digite o primeiro número: "))  
+#                b = (int("Digite o segundo número: "))  
+#                   
+#                result = soma (a, b)   
+#                print(10*"-")   ------------> Vai ser importante para separação de comando
+#                   
+#                -----////////////////////////////-----  
+# 
+#                 def lenmatriz(matriz):
+#                 matriz = [[],[],[],
+#                           [],[],[],
+#                           [],[],[]]
+#                 tamanho = 0
+#   
+#                 for linha in range(len(matriz)):
+#                     for coluna in range(len(matriz[linha]))         
+#                       tamanho+=1
+#                
+#                     return tamanho
+#                 print(lenmatriz(matriz))
+#                  
+#                -----////////////////////////////-----  
+# 
+#                print(__name__) -> ele mostra o arquivo que esta manipulando
+#                import "nome do arquivo" import "nome da função que esta buscando"
+#                
+# 
+# 
+# 
+# 
+# 
+# 
+# 
+# 
+# 
+# 
+# 
+# 
+
+# #
